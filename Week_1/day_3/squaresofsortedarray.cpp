d7ec0876ca25c8a3908950cb1eb91f5491d32db8
SamarthDhillon12 <samarthdhillon18@gmail.com>
2026-06-05 22:02:28 +0530

    Add files via upload

---
+++ Week_1/day_3/squaresofsortedarray.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> result(n);
+
+        int left = 0;
+        int right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (abs(nums[left]) > abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+
+        return result;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {-4, -1, 0, 3, 10};
+
+    vector<int> ans = sol.sortedSquares(nums);
+
+    for (int x : ans) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
