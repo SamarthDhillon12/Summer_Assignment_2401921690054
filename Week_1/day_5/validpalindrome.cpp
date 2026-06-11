c414afffe485b69b7f0cbdf8321eee121902c6b7
SamarthDhillon12 <samarthdhillon18@gmail.com>
2026-06-11 20:31:10 +0530

    Add files via upload

---
+++ Week_1/day_5/validpalindrome.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+bool isPalindrome(string s) {
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+        while (left < right && !isalnum(s[left]))
+            left++;
+
+        while (left < right && !isalnum(s[right]))
+            right--;
+
+        if (tolower(s[left]) != tolower(s[right]))
+            return false;
+
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+int main() {
+    string s = "A man, a plan, a canal: Panama";
+
+    if (isPalindrome(s))
+        cout << "true";
+    else
+        cout << "false";
+
+    return 0;
+}
